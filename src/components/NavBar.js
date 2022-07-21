d1ed747e5d97fdd6de481a716a2bde071477ab60
albert <albert>
2022-07-21 12:38:06 +0200

    bef

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
@@ -6,7 +5,8 @@ import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import { HashLink } from "react-router-hash-link";
-import { BrowserRouter as Router } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
